114187b6f69a4b92079cea9e957ad0a2fccd2d27
marcoseduardoss <marcoseduardoss@gmail.com>
2020-05-06 17:14:47 -0300

    Removed comments not utilized

--- mini-mvc/src/main/java/br/me/mvc/controle/ServletControlador.java
+++ mini-mvc/src/main/java/br/me/mvc/controle/ServletControlador.java
@@ -13,71 +13,61 @@ import br.me.mvc.controle.acoes.Action;
 import br.me.mvc.excecoes.ControllerMvcException;
 import br.me.mvc.util.JavaReflectionUtil;
 
-
-@WebServlet(name = "ServletControlador", urlPatterns = {"/controllerServletPublico", "/restrito/controllerServlet"}) 
-@WebInitParam(name="package-actions-mvc", value="br.me.agenda.controle.acoes")
-//@WebServlet(value = "/controllerServletPublico")
-//@WebServlet("/restrito/controllerServlet")
+@WebServlet(name = "ServletControlador", urlPatterns = { "/controllerServletPublico", "/restrito/controllerServlet" })
 @SuppressWarnings("serial")
 public class ServletControlador extends HttpServlet {
 
-	private void controlarAcessoAoModelo(HttpServletRequest req,
-			HttpServletResponse res) 
-                throws ServletException, IOException {
-            
-            //alteral o encondig da requisicao para utf-8
-            req.setCharacterEncoding("UTF-8");            
-                
-            try {
-                
-                String className = req.getParameter("controle");
-                String realPath = req.getServletContext().getRealPath("/WEB-INF/classes");
-                
-                Action comando = Action.newInstance(className, realPath);
+	private void controlModelAccess(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 
-                //aplica a logica de negocio
-                String paginaRedirecionada = comando.executa(req, res);
-                
-                String url = "./"+paginaRedirecionada;
-                
-                //altera o encoding da repsosta para utf-8
-                res.setCharacterEncoding("UTF-8");
-                res.setContentType("text/html;charset=UTF-8");
-                
-//                if(paginaRedirecionada.equals("pagina_principal.jsp")) 
-//                    throw new Exception("teste");
-                
-                //redireciona 
-                if(comando.isRedirect())
-                    res.sendRedirect(res.encodeRedirectURL(url));
-                else
-                    req.getRequestDispatcher(url).forward(req, res);
-                
-                
-            } catch (Exception e) {
-                //imprime no console o erro ocorrido
-                String msg = "Erro na classe ServletContrador. ";
-                msg += "Detalhes do erro: "+e.getMessage();        
-                System.out.println(msg);            
-                throw new ControllerMvcException(msg, e);
-            } 
+		// update encondig
+		req.setCharacterEncoding("UTF-8");
+
+		try {
+
+			String className = req.getParameter("controle");
+
+			String realPath = req.getServletContext().getRealPath("/WEB-INF/classes");
+
+			Action comando = Action.newInstance(className, realPath);
+
+			// execute business logic
+			String paginaRedirecionada = comando.executa(req, res);
+
+			String url = "./" + paginaRedirecionada;
+
+			// update encoding of the response to utf-8
+			res.setCharacterEncoding("UTF-8");
+
+			res.setContentType("text/html;charset=UTF-8");
+
+			// redireciona
+			if (comando.isRedirect())
+				res.sendRedirect(res.encodeRedirectURL(url));
+			else
+				req.getRequestDispatcher(url).forward(req, res);
+
+		} catch (Exception e) {
+
+			String msg = "ServletContrador Error";
+
+			msg += e.getMessage();
+
+			throw new ControllerMvcException(msg, e);
+		}
 	}
 
-	protected void doPost(HttpServletRequest request,
-			HttpServletResponse response) 
-                throws ServletException, IOException {
-            
-		controlarAcessoAoModelo(request, response);
-	
-    }
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-	
-	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) 
-                throws ServletException, IOException {
-            
-		controlarAcessoAoModelo(request, response);
-	
-    }
+		controlModelAccess(request, response);
+
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		controlModelAccess(request, response);
+
+	}
 
 }
